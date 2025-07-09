012b1287df0bcb53532b9026a289a59a9ea63bb0
George Zamfirescu <George.Zamfirescu@softwire.com>
2025-07-09 14:06:31 +0300

    Can now get all the books

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -1,13 +1,66 @@
 import { Router, Request, Response } from 'express';
+import * as tedious from 'tedious';
+import dotenv from 'dotenv'
+import { Book } from '../entities/book';
+
+dotenv.config()
 
 class BookController {
     router: Router;
-
+    sqlConfiguration: tedious.ConnectionConfiguration = {
+      server: 'localhost',
+      options: {
+        trustServerCertificate: true
+      },
+      authentication: {
+        type: 'default',
+        options: {
+          userName: 'GeoZamExtra',
+          password: process.env.SQL_PASSWD,
+          trustServerCertificate: true,
+        }
+      }
+    };
+    connection: tedious.Connection;
+    
     constructor() {
         this.router = Router();
         this.router.get('/:id', this.getBook.bind(this));
-
+        this.router.get('/', this.getBooks.bind(this));
         this.router.post('/', this.createBook.bind(this));
+        
+        this.connection = new tedious.Connection(this.sqlConfiguration);
+        this.connection.on('connect', (err) => {
+          if (err) {
+            console.log('Error ', err);
+          }
+        });
+        
+        this.connection.connect();
+    }
+    
+    getBooks(req: Request, res: Response) {
+        const request = new tedious.Request('select * from bookish.dbo.books', (err, rowCount) => {
+          if (err) {
+            console.log(err);
+          }
+        });
+        
+        const books = [];
+        
+        request.on('row', columns => {
+          const row: any = {};
+          columns.forEach(column => {
+            row[column.metadata.colName] = column.value;
+          });
+          books.push(new Book(row.BookId, row.Title, row.Author, row.ISBN, row.AmountOwned));
+        });
+        
+        request.on('requestCompleted', () => {
+          res.status(200).send(JSON.stringify(books)); 
+        });
+        
+        this.connection.execSql(request);
     }
 
     getBook(req: Request, res: Response) {
